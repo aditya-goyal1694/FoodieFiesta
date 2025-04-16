d3249c8e16dd11c88133c64db60663bb4ded5081
Aditya Goyal <adityamr.1694@gmail.com>
2025-04-16 19:06:36 +0530

    credentials and methods updated for production

--- backend/foodieFiesta.sql
+++ backend/foodieFiesta.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE  IF NOT EXISTS `foodiefiesta`;
-USE `foodiefiesta`;
+CREATE DATABASE IF NOT EXISTS `if0_38763513_foodiedb`;
+USE `if0_38763513_foodiedb`;
 
 DROP TABLE IF EXISTS `food_items`;
 
@@ -8,8 +8,7 @@ CREATE TABLE `food_items` (
   `name` varchar(255) DEFAULT NULL,
   `price` decimal(10,2) DEFAULT NULL,
   PRIMARY KEY (`item_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 INSERT INTO food_items (item_id, name, price) VALUES
 (1, 'Margherita Pizza', 299.00),
@@ -47,22 +46,15 @@ INSERT INTO food_items (item_id, name, price) VALUES
 (33, 'Pani Puri', 59.00),
 (34, 'Peri Peri Fries', 79.00);
 
-
 DROP TABLE IF EXISTS `order_tracking`;
 
 CREATE TABLE `order_tracking` (
   `order_id` int NOT NULL,
   `status` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`order_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 INSERT INTO `order_tracking` VALUES (40,'delivered'),(41,'in transit');
-
---
--- Table structure for table `orders`
---
 
 DROP TABLE IF EXISTS `orders`;
 
@@ -71,82 +63,9 @@ CREATE TABLE `orders` (
   `item_id` int NOT NULL,
   `quantity` int DEFAULT NULL,
   `total_price` decimal(10,2) DEFAULT NULL,
-  PRIMARY KEY (`order_id`,`item_id`),
+  PRIMARY KEY (`order_id`, `item_id`),
   KEY `orders_ibfk_1` (`item_id`),
   CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`item_id`) REFERENCES `food_items` (`item_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-
---
--- Dumping data for table `orders`
---
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 INSERT INTO `orders` VALUES (40,1,2,598.00),(40,3,1,249.00),(41,4,3,807.00),(41,6,2,258.00),(41,9,4,316.00);
-
-
-DELIMITER ;;
-CREATE DEFINER=`root`@`localhost` FUNCTION `get_price_for_item`(p_item_name VARCHAR(255)) RETURNS decimal(10,2)
-    DETERMINISTIC
-BEGIN
-    DECLARE v_price DECIMAL(10, 2);
-    
-    -- Check if the item_name exists in the food_items table
-    IF (SELECT COUNT(*) FROM food_items WHERE name = p_item_name) > 0 THEN
-        -- Retrieve the price for the item
-        SELECT price INTO v_price
-        FROM food_items
-        WHERE name = p_item_name;
-        
-        RETURN v_price;
-    ELSE
-        -- Invalid item_name, return -1
-        RETURN -1;
-    END IF;
-END ;;
-DELIMITER ;
-
-DELIMITER ;;
-CREATE DEFINER=`root`@`localhost` FUNCTION `get_total_order_price`(p_order_id INT) RETURNS decimal(10,2)
-    DETERMINISTIC
-BEGIN
-    DECLARE v_total_price DECIMAL(10, 2);
-    
-    -- Check if the order_id exists in the orders table
-    IF (SELECT COUNT(*) FROM orders WHERE order_id = p_order_id) > 0 THEN
-        -- Calculate the total price
-        SELECT SUM(total_price) INTO v_total_price
-        FROM orders
-        WHERE order_id = p_order_id;
-        
-        RETURN v_total_price;
-    ELSE
-        -- Invalid order_id, return -1
-        RETURN -1;
-    END IF;
-END ;;
-DELIMITER ;
-
-
-DELIMITER ;;
-CREATE DEFINER=`root`@`localhost` PROCEDURE `insert_order_item`(
-  IN p_food_item VARCHAR(255),
-  IN p_quantity INT,
-  IN p_order_id INT
-)
-BEGIN
-    DECLARE v_item_id INT;
-    DECLARE v_price DECIMAL(10, 2);
-    DECLARE v_total_price DECIMAL(10, 2);
-
-    -- Get the item_id and price for the food item
-    SET v_item_id = (SELECT item_id FROM food_items WHERE name = p_food_item);
-    SET v_price = (SELECT get_price_for_item(p_food_item));
-
-    -- Calculate the total price for the order item
-    SET v_total_price = v_price * p_quantity;
-
-    -- Insert the order item into the orders table
-    INSERT INTO orders (order_id, item_id, quantity, total_price)
-    VALUES (p_order_id, v_item_id, p_quantity, v_total_price);
-END ;;
-DELIMITER ;
